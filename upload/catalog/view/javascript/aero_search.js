b64581382fb3846784003c10dbe67b8e2a788753
Webpolka <webpolka@gmail.com>
2025-12-12 20:02:24 +0300

    12.12.2025

--- upload/catalog/view/javascript/aero_search.js
+++ upload/catalog/view/javascript/aero_search.js
@@ -9,41 +9,51 @@ function cartAdd(product_id, quantity, imageSelector) {
   })
     .then((res) => res.json())
     .then((data) => {
+      // Если сервер вернул редирект (товар с опциями)
+      if (data.redirect) {
+        window.location.href = data.redirect;
+        return; // прекращаем выполнение функции
+      }
+
+      // Если успех — уведомление и обновление корзины
       if (data.success) {
         lsNotify("success", data.success);
-
-        //  обновляем кнопку корзины
         updateHeaderCart();
       }
+
+      // Ошибка (не редирект)
       if (data.error) {
         lsNotify("error", data.error);
       }
-    });
-}
-
-
-function updateHeaderCart() {
-    fetch("index.php?route=common/cart/info", {
-        method: "GET",
-        headers: { "X-Requested-With": "XMLHttpRequest" }
     })
-    .then(res => res.text())
-    .then(html => {
-        // создаём временный элемент, чтобы вытащить текст кнопки
-        const tempDiv = document.createElement('div');
-        tempDiv.innerHTML = html;
-
-        // OpenCart стандартно отдаёт мини-корзину с кнопкой, ищем её
-        const newBtn = tempDiv.querySelector('button.btn.dropdown-toggle');
-        if (newBtn) {
-            const headerBtn = document.querySelector('button.btn.dropdown-toggle');
-            if (headerBtn) {
-                headerBtn.innerHTML = newBtn.innerHTML;
-            }
-        }
+    .catch((err) => {
+      console.error("Error in cartAdd:", err);
+      lsNotify("error", "An unexpected error occurred.");
     });
 }
 
+// Update cart
+function updateHeaderCart() {
+  fetch("index.php?route=common/cart.info", {
+    method: "GET",
+    headers: { "X-Requested-With": "XMLHttpRequest" },
+  })
+    .then((res) => res.text())
+    .then((html) => {
+      // создаём временный контейнер
+      const tempDiv = document.createElement("div");
+      tempDiv.innerHTML = html;
+
+      // вытаскиваем полный блок корзины из AJAX
+      const newCart = tempDiv.querySelector(".dropdown.d-grid");
+      const currentCart = document.querySelector("#cart");
+      const correntCartGrid = currentCart.querySelector(".dropdown.d-grid");
+
+      if (newCart && currentCart) {
+        correntCartGrid.innerHTML = newCart.innerHTML;
+      }
+    });
+}
 
 // Notife Lite function
 function lsNotify(type, text, delay = 3000) {
@@ -62,6 +72,28 @@ function lsNotify(type, text, delay = 3000) {
     toast.removeClass("show");
     setTimeout(() => toast.remove(), 300);
   }, delay);
+}
+
+// Render review stars
+function renderAeroRating(rating) {
+  const StarFilled = `
+<svg viewBox="0 0 1024 1024" class="star star-filled" width="15" height="15">
+  <path fill="orange" d="m908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5c-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 0 0 .6 45.3l183.7 179.1l-43.4 252.9a31.95 31.95 0 0 0 46.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2c17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9l183.7-179.1c5-4.9 8.3-11.3 9.3-18.3c2.7-17.5-9.5-33.7-27-36.3"/>
+</svg>
+`;
+  const StarEmpty = `
+<svg viewBox="0 0 1024 1024" class="star star-empty" width="15" height="15">
+  <path fill="orange" d="m908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5c-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 0 0 .6 45.3l183.7 179.1l-43.4 252.9a31.95 31.95 0 0 0 46.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2c17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9l183.7-179.1c5-4.9 8.3-11.3 9.3-18.3c2.7-17.5-9.5-33.7-27-36.3M664.8 561.6l36.1 210.3L512 672.7L323.1 772l36.1-210.3l-152.8-149L417.6 382L512 190.7L606.4 382l211.2 30.7z"/>
+</svg>
+`;
+
+  let html = '';
+
+  for (let i = 1; i <= 5; i++) {
+    html += i <= rating ? StarFilled : StarEmpty;
+  }
+
+  return html;
 }
 
 // Aero search function
@@ -159,10 +191,16 @@ const AeroSearch = (function () {
                       '"></div>';
                   }
                   // show name & extra_info
-                  html += '<div class="product-name">' + product.name;
+                  html += '<div class="product-main">';
+
+                  html += '<div class="product-main-top">';
+                  html += '<span class="product-main-name">' + product.name + '</span>';
+                  html += '<span class="product-main-stars">' + renderAeroRating(product.rating) + '</span>';
+                  html += "</div>";
+
                   if (show_description == 1) {
                     html += "<p>" + product.extra_info + "</p>";
-                  }
+                                    }
                   html += "</div>";
 
                   html += "</a>";
@@ -213,7 +251,7 @@ const AeroSearch = (function () {
 
                 $(".aero-search ul").html(html);
               }
-              
+
               $(".aero-search").css("display", "block");
 
               return false;
